3848153403205c37501f0846311ea3fd2d8aef93
soufyane <soufyanee.blg@gmail.com>
2026-03-16 03:05:11 +0300

    Add the password hashing for the users

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
 from accounts.models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
         fields = "__all__"
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        return user
